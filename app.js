40b326e328fc64beb55124335a4e18016cccafc9
Christophe <christophe.rosset@4d.com>
2013-08-08 14:36:51 +0200

    added socket.io to express (socket.io 0.9.x not supported fallback to 0.8.x)

--- app.js
+++ app.js
@@ -9,29 +9,45 @@ var express = require('express')
   , http = require('http')
   , path = require('path');
 
+var port = process.env.PORT || 3000;
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/app/views');
-app.set('view engine', 'ejs');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
-app.use(express.session());
-app.use(app.router);
-app.use(express.static(path.join(__dirname,'app','public','src')));
+app.configure(function(){
+    app.set('port', port);
+    app.set('views', __dirname + '/app/views');
+    app.set('view engine', 'ejs');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.cookieParser('your secret here'));
+    app.use(express.session());
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname,'app','public','src')));
+});
 
 // development only
-if ('development' == app.get('env')) {
+app.configure('development', function(){
   app.use(express.errorHandler());
-}
+});
 
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
+var server = http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
+});
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function (socket) {
+  io.sockets.emit('this', { will: 'be received by everyone'});
+
+  socket.on('private message', function (from, msg) {
+    console.log('I received a private message by ', from, ' saying ', msg);
+  });
+
+  socket.on('disconnect', function () {
+    io.sockets.emit('user disconnected');
+  });
 });
---
+++ app/public/src/js/custom/common.js
@@ -0,0 +1,59 @@
+define(function(){
+    
+    var common;
+    
+    common = {
+        
+        stage : {
+            width : 600,
+            height : 500
+        },
+        
+        ballConst : {
+            radius           : 15,
+            mass             : 1.3,
+            gravity          : 1,
+            elasticity       : 0.98,
+            friction         : 0.94,
+            lifetime         : Infinity,
+            options          : {
+                aging:true,
+                bouncingColor:'#000060',
+                bouncingRate: 20,
+                glowingColor:'#9922DD',
+                glowingRate:'#9922DD',
+                explodingRadius:80,
+                explodingRate:40,
+                explodingAlpha:true
+            }
+        },
+        
+        isRemoteTiltEnabled : function(){
+            if(window.location.href.indexOf('#tiltremote') > -1)
+                return true;
+            else
+                return false;
+        },
+                
+        getRandomColor : function(){
+            var color = '#000000';
+            while(color === '#000000'){
+                color = '#'+Math.floor(Math.random()*16777215).toString(16);
+            }
+            return color;
+        },
+                
+        getRandomPositionAndSpeedInBounds : function(){
+            return {
+                x : Math.random()*this.stage.width,
+                y : Math.random()*this.stage.height,
+                velocityX : Math.random()*10,
+                velocityY : Math.random()*10
+            };
+        }
+        
+    };
+    
+    return common;
+    
+});
