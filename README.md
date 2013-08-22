3affca6d2573d85b0195ccda090660d1be4aa41f
Christophe <christophe.rosset@4d.com>
2013-08-22 17:47:59 +0200

    LICENSE + README + flush unecessary sockets

--- README.md
+++ README.md
@@ -1,4 +1,37 @@
 TOPHEMAN PLAYGROUND V2
 ======================
 
-Comming soon a real readme as well as a demo ...
+##Intro
+
+Try it at : [http://topheman-playground.herokuapp.com](http://topheman-playground.herokuapp.com/) (heroku instance).
+
+Presentation : [http://slid.es/topheman/topheman-playground](http://slid.es/topheman/topheman-playground)
+
+Once you snapped the QR-code with your phone, take control of the bouncing ball on your desktop screen, like a Wiimote, tilting your phone as you would do on a labyrinth game.
+
+* Connect with multiple mobile devices
+* Watch from multiple desktop/laptop screens
+
+You can even use the devicemotion emulator to control the ball from your desktop.
+
+Not a game, more of an experiment that could lead to a game …
+
+Thanks to [remote-tilt](http://remote-tilt.com/) for the emulator.
+
+##Install
+
+* `npm install`
+* `node app.js 192.168.1.2:3000` (192.168.1.2:3000 being the public IP:PORT of your node server - port by default at 3000 - this is used to create the QR-Code)
+
+##Next
+
+I coded the [v1](https://github.com/topheman/playground1) about a year ago, and now I decided to refactor it so that it could be a sort of a boilerplate for some other games.
+
+Take it, make your own games and tell me about it !
+
+
+##Notes
+
+My heroku instance doesn't support WebSockets, so socket.io fallbacks to xhr-polling. If you want to test a version with real WebSockets, you can try : [http://playground.topheman.kd.io:3000](http://playground.topheman.kd.io:3000/) although it only stays up for 20 min (after the VM goes asleep - so tweet me or whatever so I could lauch it).
+
+When you'll browse the code, you'll see that there is a connection/disconnetion routine on the sockets which makes sure to disconnect any mobile client which has more than one socket opened (to prevent multiple dead balls). This was a tricky part, playing with the express session and the socket.io handshake :-) …
--- app/logic/playground.io.js
+++ app/logic/playground.io.js
@@ -19,14 +19,32 @@ exports.init = function(io) {
      * @param {String} socketId
      */
     var disconnectMobile = function(sessionId, socketId){
+        var i;
+        console.log('>disconnecting mobile');
         //before, check if the socket is still active to prevent loops the client.disconnect() callback and calling this method directly if a duplicate socket on the same session is spotted
         if(mobileSockets[sessionId] && mobiles[socketId]){
             //alert the desktops to remove the mobile - passing the socket id
             io.of('/desktop').emit('desktop-remove-mobile', {id: socketId});
-            //flush the references to this connexion
+            //flush the references to this connexion in mobiles
+            console.log('>>flushing mobiles with socketId '+socketId);
             delete mobiles[socketId];
-            console.log('removed mobile socket.id : ', socketId, 'sessionId : ',sessionId);
+            console.log('<<flushing mobiles with socketId');
+            //flush the references to this connexion in mobilesSockets
+            console.log('>>flushing mobileSockets with socketId '+socketId);
+            for(i = 0;i < mobileSockets[sessionId].length; i++){
+                if(mobileSockets[sessionId][i].id === socketId){
+                    mobileSockets[sessionId].splice(i,1);
+                    console.log('>>>flushed mobileSockets[sessionId] with '+socketId);
+                    break;
+                }
+            }
+            if(mobileSockets[sessionId].length === 0){
+                delete mobileSockets[sessionId];
+                    console.log('>>>mobileSockets[sessionId] empty > deleted');
+            }
+            console.log('<<flushing mobileSockets with socketId '+socketId);
         }
+        console.log('<disconnecting mobile');
     };
     
     /**
@@ -59,13 +77,14 @@ exports.init = function(io) {
     });
 
     io.of('/mobile').on('connection',function(socket){
+        console.log('>mobile connect');
         //check if a mobile already has a session opened, to close its socket
         if(mobileSockets[socket.handshake.sessionID]){
-            console.log('MOBILE ALREADY CONNECTED on this sessionId '+socket.handshake.sessionID, 'disconnecting all other sockets connected to this session');
+            console.log('>>MOBILE ALREADY CONNECTED on this sessionId '+socket.handshake.sessionID, 'disconnecting all other sockets connected to this session');
             //disconnect all the other sockets of this sessionId
             loopThroughMobileSocketsBySessionId(socket.handshake.sessionID, function(mobileSocket,sessionId, socketId){
                 if(sessionId === socket.handshake.sessionID && socketId !== socket.id){
-                    console.log('emiting a force-disconnect to socket.id : ',socketId);
+                    console.log('>>emiting a force-disconnect to socket.id : ',socketId);
                     //tell the mobile client to disconnect
                     mobileSocket.emit('force-disconnect',{});
                     //flush the references to this mobile socket
@@ -83,12 +102,13 @@ exports.init = function(io) {
         var color = common.getRandomColor();
         //alert the mobile it's connected with its color
         socket.emit('mobile-connected', {color: color});
-        console.log('mobile-connected with socket.id : ', socket.id);
+        console.log('>>mobile-connected with socket.id : ', socket.id);
         //alert the desktops to add the mobile with this color and position
         var infos = common.getRandomPositionAndSpeedInBounds();
         infos.id = socket.id;
         infos.color = color;
         io.of('/desktop').emit('desktop-add-mobile', infos);
+        console.log('<mobile connect');
         
         socket.on('mobile-infos', function(data) {
             //prevent lost connection still emitting to crash the server
@@ -104,27 +124,31 @@ exports.init = function(io) {
         
     
         socket.on('disconnect',function(data){
-            console.log('mobile trying to disconnect');
+            console.log('>mobile client - .disconnect()');
             disconnectMobile(socket.handshake.sessionID, socket.id);
+            console.log('<mobile client - .disconnect()');
         });
         
     });
     
     io.of('/desktop').on('connection',function(socket){
+        console.log('>desktop connect');
         desktops[socket.id] = {
             timeStamp : Date.now()
         };
-        console.info('desktop connected');
+        console.info('>>desktop connected');
         //alert all another desktop connected (in order to force mobiles to reconnect)
         socket.broadcast.emit('desktop-connected', {});
         io.of('/mobile').emit('desktop-connected', {});
+        console.log('<desktop connect');
     
         socket.on('disconnect',function(data){
-            console.log('desktop trying to disconnect');
+            console.log('>desktop client - .disconnect()');
             if (desktops[socket.id] !== null) {
                 delete desktops[socket.id];
-                console.log('remove desktop', socket.id);
+                console.log('remove desktop with socketId : ', socket.id);
             }
+            console.log('<desktop client - .disconnect()');
         });
     
     });
